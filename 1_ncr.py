c12ea056fe58374ee71d76661c816270c7a51900
mohits44 <101943239+mohits44@users.noreply.github.com>
2023-08-27 05:03:47 +0530

    Update 1_ncr.py

--- 1_ncr.py
+++ 1_ncr.py
@@ -1,1 +1,12 @@
-
+# a good demonstration for functions
+def nCr(n,r):
+    res=myFactorial(n)/(myFactorial(r)*myFactorial(n-r))
+    return res
+def myFactorial(n):
+    fact=1
+    for i in range (1,n+1):
+        fact=fact*i
+        
+    return fact
+    
+print(nCr(10,5))
